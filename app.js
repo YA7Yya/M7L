d7dc8c8ab0431e9a5ac8ba8ae4a59b08dd87da12
YA7Yya <yahyazaitoon777@gmail.com>
2024-07-14 05:32:21 +0300

    as

--- app.js
+++ app.js
@@ -29,6 +29,7 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, "public")));
 mongoose
   .connect(
