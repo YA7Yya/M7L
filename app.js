25f5919cf73a8aa936e632b982929e5c8a77da1f
YA7Yya <yahyazaitoon777@gmail.com>
2024-07-15 03:39:36 +0300

    gfgf

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
-// const cors = require("cors");
-// const corsConfig = {
-//   origin: "*",
-//   credential: true,
-//   methods: ["GET", "POST", "PUT", "DELETE"],
-// };
+const cors = require("cors");
+const corsConfig = {
+  origin: "*",
+  credential: true,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+};
 let port = 80;
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
@@ -31,10 +31,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(cors(corsConfig));
 mongoose.connect(process.env.DB).then(() => {
   console.log("DB Started Successfully");
 });
-// app.use(cors(corsConfig));
 let day = 3600000 * 24;
 const STORE = new SessionStore({
   uri: process.env.DB,
