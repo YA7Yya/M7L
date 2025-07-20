81c67bc00e1bfee5d254c27c10b6c17aed418eff
YA7Yya <yahyazaitoon777@gmail.com>
2025-07-20 23:42:39 +0300

    a

--- app.js
+++ app.js
@@ -776,12 +776,10 @@ app.post("/sale/add", (req,res) =>{
  Sales.Sale.estimatedDocumentCount().then((countedDoc) => {
     Sales.newSale(req.body.PNAME,req.body.QUANTITY,req.body.PRICE,req.body.PNOTES,req.body.TOTAL,countedDoc + 1,req.session.username,req.session.username).then((result)=>{
   console.log(req.body);
-      res.redirect("/")
+      res.redirect("/allreceipts")
 
 })
-  }).catch((err) => {
-    
-  });
+  })
 
    
 })
